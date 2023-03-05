3689676aa1a71871cf38198f1e56ac2f4c6892ce
IvoPetk0v <ipetk0v@students.softuni.bg>
2023-03-05 17:07:30 +0200

    upload

---
+++ Exercises_Entity_Relations/task01-short structure/P01_StudentSystem/Data/Models/Resource.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.AccessControl;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Url { get; set; } = null!; // not unicode
+
+
+        [Required]
+        public ResourceType ResourceType { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Course))]
+        public int CourseId { get; set; }
+
+        public virtual Course Course { get; set; } = null!;
+
+    }
+}
---
+++ Exercises_Entity_Relations/task01-short structure/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,63 @@
+﻿namespace P01_StudentSystem.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Models;
+
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> Homeworks { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentsCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=StudentSystem;Integrated Security=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        // Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<StudentCourse>(entity =>
+            {
+                entity.HasKey(sc => new { sc.StudentId, sc.CourseId });
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(s => s.PhoneNumber)
+                .IsUnicode(false)
+                .HasMaxLength(10);
+
+            });
+            modelBuilder.Entity<Resource>(entity =>
+            {
+                entity.Property(r => r.Url)
+                .IsUnicode(false);
+
+            });
+        }
+    }
+}
