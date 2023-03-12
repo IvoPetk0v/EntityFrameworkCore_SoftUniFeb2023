2916a96532e3d85afffae490a6969569d77cb763
IvoPetk0v <ipetk0v@students.softuni.bg>
2023-03-12 14:53:07 +0200

    upload tasks

--- Exercises_Advanced_Querying/BookShop/StartUp.cs
+++ Exercises_Advanced_Querying/BookShop/StartUp.cs
@@ -1,15 +1,38 @@
 ﻿namespace BookShop
 {
+    using Castle.Core.Internal;
     using Data;
     using Initializer;
+    using System.Linq;
 
     public class StartUp
     {
         public static void Main()
         {
-            using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+            using var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+
+            string input = Console.ReadLine();
+            Console.Write(GetBooksByAgeRestriction(context, input));
+
         }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var bookTitles = context.Books
+                .ToArray()
+                .Where(b => !command.IsNullOrEmpty() &&
+                b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, bookTitles);
+
+        }
+
+
     }
 }
 
