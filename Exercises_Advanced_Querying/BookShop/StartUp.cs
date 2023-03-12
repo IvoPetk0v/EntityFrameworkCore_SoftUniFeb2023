cfbc6c20a3b9d76400ce1df9f05c403acd7ca568
IvoPetk0v <ipetk0v@students.softuni.bg>
2023-03-12 15:40:13 +0200

    upload new tasks

--- Exercises_Advanced_Querying/BookShop/StartUp.cs
+++ Exercises_Advanced_Querying/BookShop/StartUp.cs
@@ -1,9 +1,12 @@
 ﻿namespace BookShop
 {
+    using System.Linq;
+
+    using Models.Enums;
     using Castle.Core.Internal;
     using Data;
     using Initializer;
-    using System.Linq;
+    using System.Text;
 
     public class StartUp
     {
@@ -13,8 +16,7 @@
             DbInitializer.ResetDatabase(context);
 
 
-            string input = Console.ReadLine();
-            Console.Write(GetBooksByAgeRestriction(context, input));
+            Console.Write(GetBooksByPrice(context));
 
         }
 
@@ -30,6 +32,34 @@
 
             return string.Join(Environment.NewLine, bookTitles);
 
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+
+            var booksTitle = context.Books
+                .Where(b => b.Copies < 5000 && b.EditionType == EditionType.Gold)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+            return string.Join(Environment.NewLine, booksTitle);
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            //Return in a single string all titles and prices of books with a price higher than 40, each on a new row in the format given below. Order them by price descending...
+
+            var books = context.Books
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .Select(b => new { b.Title, b.Price })
+                .ToArray();
+            var sb = new StringBuilder();
+            foreach (var b in books)
+            {
+                sb.AppendLine($"{b.Title} - ${b.Price:f2}");
+            }
+            return sb.ToString().TrimEnd();
         }
 
 
