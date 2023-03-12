8404a1bc8baa866c123f20975a8a3963a7771690
IvoPetk0v <ipetk0v@students.softuni.bg>
2023-03-12 20:13:01 +0200

    upload new tasks

--- Exercises_Advanced_Querying/BookShop/StartUp.cs
+++ Exercises_Advanced_Querying/BookShop/StartUp.cs
@@ -1,22 +1,28 @@
 ﻿namespace BookShop
 {
+    using Microsoft.EntityFrameworkCore;
+
     using System.Linq;
+    using System.Collections.Immutable;
 
     using Models.Enums;
     using Castle.Core.Internal;
     using Data;
     using Initializer;
     using System.Text;
+    using Microsoft.EntityFrameworkCore.Query.Internal;
+    using System.Globalization;
 
     public class StartUp
     {
         public static void Main()
         {
             using var context = new BookShopContext();
-            DbInitializer.ResetDatabase(context);
+            // DbInitializer.ResetDatabase(context);
 
+            string input = Console.ReadLine();
 
-            Console.Write(GetBooksByPrice(context));
+            Console.Write(GetBookTitlesContaining(context, input));
 
         }
 
@@ -47,7 +53,6 @@
 
         public static string GetBooksByPrice(BookShopContext context)
         {
-            //Return in a single string all titles and prices of books with a price higher than 40, each on a new row in the format given below. Order them by price descending...
 
             var books = context.Books
                 .Where(b => b.Price > 40)
@@ -62,6 +67,81 @@
             return sb.ToString().TrimEnd();
         }
 
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+
+            var books = context.Books.ToArray()
+                .Where(b => b.ReleaseDate.HasValue && b.ReleaseDate.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title);
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+
+            var bookCategories = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(c => c.ToLower())
+                .ToArray();
+
+            var books = context.Books
+                .Where(b => b.BookCategories.Any(bc => bookCategories.Contains(bc.Category.Name.ToLower())))
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books);
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var dateFormated = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var books = context.Books
+                 .Where(b => b.ReleaseDate < dateFormated)
+                 .OrderByDescending(b => b.ReleaseDate)
+                 .Select(b => new
+                 {
+                     b.Title,
+                     b.EditionType,
+                     b.Price
+                 })
+                 .ToArray();
+
+            var sb = new StringBuilder();
+
+            foreach (var b in books)
+            {
+                sb.AppendLine($"{b.Title} - {b.EditionType} - ${b.Price:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+
+            var authors = context.Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .Select(a => a.FirstName + " " + a.LastName)
+                .OrderBy(a => a)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, authors);
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            string subStr = input.ToLower();
+            var books = context.Books
+                .Where(b => b.Title.ToLower().Contains(subStr))
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToArray();
+            return string.Join(Environment.NewLine, books);
+        }
 
     }
 }
