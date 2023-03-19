8536a4535e76ba6d8d17b52a59877031f443c2cf
IvoPetk0v <ipetk0v@students.softuni.bg>
2023-03-19 14:52:55 +0200

    upload new tasks

---
+++ Exercises_JSON_Processing/01/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace ProductShop
+{
+    using AutoMapper;
+
+    using DTOs.Import;
+    using Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //Users
+            this.CreateMap<ImportUserDto, User>();
+
+            //Products
+            this.CreateMap<ImportProductDto, Product>();
+
+
+        }
+    }
+}
---
+++ Exercises_JSON_Processing/01/ProductShop/StartUp.cs
@@ -0,0 +1,62 @@
+﻿namespace ProductShop
+{
+    using AutoMapper;
+    using Newtonsoft.Json;
+    using ProductShop.Data;
+    using ProductShop.DTOs.Import;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+
+            string input = File.ReadAllText(@"../../../Datasets/products.json");
+
+            string result = ImportProducts(context, input);
+            Console.WriteLine(result);
+        }
+
+        private static IMapper CreateMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var mapper = CreateMapper();
+            var userDtos = JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+            var validUsers = new HashSet<User>();
+
+            foreach (var userDto in userDtos!)
+            {
+                var user = mapper.Map<User>(userDto);
+                validUsers.Add(user);
+            }
+
+            context.AddRange(validUsers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validUsers.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var mapper = CreateMapper();
+            var productDtos = JsonConvert.DeserializeObject<ImportProductDto[]>(inputJson);
+            Product[] validProducts = mapper.Map<Product[]>(productDtos);
+            context.AddRange(validProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {validProducts.Length}";
+        }
+    }
+
+
+
+
+}
